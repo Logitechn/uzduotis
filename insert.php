bc0e23f595b03818a1fe5b9e731d18e0a2e3203d
Logitechn <blazysaurimas5@gmail.com>
2015-04-11 23:53:38 +0300

    prepered statement
    nesigauna birth_years ir shirt_number insert'int bind_param, nei s, nei
    i, nei b, nei d.

--- insert.php
+++ insert.php
@@ -19,20 +19,18 @@
     $birth = strip_tags(!empty(mysqli_real_escape_string($link, $_POST['birth_years'])) ? sprintf("'%s'",$_POST['birth_years']) : 'null');
     $number = strip_tags(!empty(mysqli_real_escape_string($link, $_POST['shirt_number'])) ? sprintf("'%s'",$_POST['shirt_number']) : 'null');
     
-    //$sql = "INSERT INTO players name=?"." and surname=?". ",birth_years,shirt_number) VALUES (?,?,?,?)";
-    //$sql = $link->prepare("INSERT INTO players (name,surname,birth_years,shirt_number) 
-    //VALUES(?,?,?,?)");
-    $sql = "INSERT INTO players (name,surname,birth_years,shirt_number) VALUES ('".$nam."', '".$surn."', ".$birth.", ".$number.")";
-    //$stmt = $link->prepare($sql);
-    //$sql->bind_param("ssss", $name, $surname, $birth_years, $shirt_number);
-    //$stmt->execute(array($name, $surname, $birth_years, $shirt_number));
+    //$stmt = mysqli_prepare($link, "INSERT INTO players name=?"." and surname=?". "birth_years=?". "shirt_number=?");
+    $sql = "INSERT INTO players (name,surname,birth_years) VALUES(?,?,?)";
+    //$stmt = mysqli_prepare($link, $sql);
+    //$sql = "INSERT INTO players (name,surname,birth_years,shirt_number) VALUES ('".$nam."', '".$surn."', ".$birth.", ".$number.")";
+    $stmt = $link->prepare($sql);
+    $stmt->bind_param('sss', $nam, $surn, $birth);   
+    //mysqli_stmt_bind_param($stmt, "sssd", $name, $surname, $birth_years, $shirt_number);
+    //mysqli_stmt_execute($stmt);
+    $stmt->execute();
     //$stmt->fetch();
-    if (!$link->query($sql)) 
-    {
-        die('error: ' . mysql_error());
-    }else
-    {
-        header("Location:index.php"); 
-    }
-    //$sql->close();
+    $stmt->close();
+    //mysqli_stmt_close($stmt);
     $link->close();
+    //mysqli_close($link);
+    header("Location:index.php"); 
